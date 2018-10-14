4d159b5cf96b609796f627aadf17cf22ebc04781
ys62 <ys62@yskad.local>
2018-10-14 11:22:42 +0900

    elpa

---
+++ elpa/magit-annex-20180120.1534/magit-annex-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "magit-annex" "20180120.1534" "Control git-annex from Magit" '((cl-lib "0.3") (magit "2.11.0")) :commit "44eaef7d55647d5d4bd466742b738d7a9563d07f" :url "https://github.com/magit/magit-annex" :keywords '("vc" "tools"))
